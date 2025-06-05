424c57be3b5f497fdf7983e8ad54ac280cd4629a
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-05 14:00:37 -0700

    output last letter N

--- Print Your Name/src/Main.java
+++ Print Your Name/src/Main.java
@@ -3,11 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("    J  JJJJJ  J   J");
-        System.out.println("    J  J   J  J   J");
-        System.out.println("    J  J   J  JJJJJ");
-        System.out.println("J   J  J   J  J   J");
-        System.out.println("JJJJJ  JJJJJ  J   J");
+        System.out.println("    J  JJJJJ  J   J  J   J");
+        System.out.println("    J  J   J  J   J  JJ  J");
+        System.out.println("    J  J   J  JJJJJ  J J J");
+        System.out.println("J   J  J   J  J   J  J  JJ");
+        System.out.println("JJJJJ  JJJJJ  J   J  J   J");
 
 
     }
