73fe5c276f5e2e2dcdb55506166c3f03f1db4c38
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-05 16:07:08 -0700

    added calculation for finding difference between two input numbers
    tested by outputting difference

--- SimpleCalc/src/com/simplecalc/SimpleCalc.java
+++ SimpleCalc/src/com/simplecalc/SimpleCalc.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class SimpleCalc {
     public static void main(String[] arg) {
 
-//        Initialize scanner for user input
+        //        Initialize scanner for user input
         Scanner scnr = new Scanner(System.in);
 
-//        Define and initialize two variables that collect two different numbers from user
+        //        Define and initialize two variables that collect two different numbers from user
         System.out.println("Choose your first number.");
         int num1 = scnr.nextInt();
         System.out.println("Choose your second number.");
         int num2 = scnr.nextInt();
 
-//        Define and initialize a variable that stores the sum of the two numbers.
+        //        Define and initialize a variable that stores the sum of the two numbers.
 
-        int sum = num1 + num2;
-        System.out.print(sum);
+//        int sum = num1 + num2;
+//        System.out.print(sum);
 
-//        Define and initialize a variable that stores the difference of the two numbers.
+        //        Define and initialize a variable that stores the difference of the two numbers.
 
+        int difference = num1 - num2;
+        System.out.print(difference);
 
-
-//        Define and initialize a variable that stores the product of the two numbers.
+        //        Define and initialize a variable that stores the product of the two numbers.
 
 
 
