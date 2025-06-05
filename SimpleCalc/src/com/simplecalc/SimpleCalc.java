eb5e2994fbb37fbb94be433f17dc1b107183ffa1
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-05 16:00:34 -0700

    added user prompt for two different numbers
    defined and initialized variables to hold the two numbers from input
    
    tested program by outputting number user input

--- SimpleCalc/src/com/simplecalc/SimpleCalc.java
+++ SimpleCalc/src/com/simplecalc/SimpleCalc.java
@@ -1,4 +1,5 @@
 package com.simplecalc;
+
 import java.util.Scanner;
 
 public class SimpleCalc {
@@ -6,5 +7,15 @@ public class SimpleCalc {
 
 //        Initialize scanner for user input
         Scanner scnr = new Scanner(System.in);
+
+//        Define and initialize two variables that collect two different numbers from user
+        System.out.println("Choose your first number.");
+        int num1 = scnr.nextInt();
+        System.out.println("Choose your second number.");
+        int num2 = scnr.nextInt();
+
+        System.out.println(num1);
+        System.out.println(num2);
+
     }
 }
