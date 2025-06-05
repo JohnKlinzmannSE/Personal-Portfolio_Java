0959a934cfc437e6e208c7680c5c3a0dae036022
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-05 15:54:28 -0700

    imported scanner module and initialized scanner so user input could be read

--- SimpleCalc/src/com/simplecalc/SimpleCalc.java
+++ SimpleCalc/src/com/simplecalc/SimpleCalc.java
@@ -1,5 +1,10 @@
 package com.simplecalc;
-
+import java.util.Scanner;
 
 public class SimpleCalc {
+    public static void main(String[] arg) {
+
+//        Initialize scanner for user input
+        Scanner scnr = new Scanner(System.in);
+    }
 }
