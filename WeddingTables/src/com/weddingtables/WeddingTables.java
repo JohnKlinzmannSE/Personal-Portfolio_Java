1fdf04bfb3ae0bb1e06c4298f3f3c5c578f95c39
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-06 12:03:01 -0700

    created initial program that takes user input as number of guests and calculates number of tables needed

--- WeddingTables/src/com/weddingtables/WeddingTables.java
+++ WeddingTables/src/com/weddingtables/WeddingTables.java
@@ -1,7 +1,29 @@
 package com.weddingtables;
 
+import java.util.Scanner;
+
 public class WeddingTables {
     public static void main(String[] arg) {
+
+        //      Number of guests that can sit at each table
+        int guestsPerTable = 10;
+
+        //      Get total wedding guests input from user
+        Scanner scnr = new Scanner(System.in);
+
+//        Ask user for number of guests attending wedding
+
+        int guests = scnr.nextInt();
+
+//        Calculate how many tables will be filled by the input guests and output table number
+
+        int tablesFilled = guests / guestsPerTable;
+        System.out.print(tablesFilled);
+
+//        Calculate how many tables will be needed to seat all guests
+
+        int tablesNeeded = tablesFilled % guests;
+        System.out.print(tablesNeeded);
 
     }
 }
