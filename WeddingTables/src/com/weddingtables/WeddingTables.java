85371740b22c6633fb5d0cbf551f67e21faf2a4b
JohnKlinzmannSE <208885982+JohnKlinzmannSE@users.noreply.github.com>
2025-06-06 12:48:47 -0700

    Added a greeting
    Modified output to show number of full tables, number of guests without a seat, and the total number of tables needed to seat all guests

--- WeddingTables/src/com/weddingtables/WeddingTables.java
+++ WeddingTables/src/com/weddingtables/WeddingTables.java
@@ -11,6 +11,11 @@ public class WeddingTables {
         //      Get total wedding guests input from user
         Scanner scnr = new Scanner(System.in);
 
+//        Greet user
+        System.out.println("Welcome to Wedding Planner 2025!");
+        System.out.println("The tables available at your venue can seat 10 guests each.");
+        System.out.println("Let us help you plan the perfect wedding by making sure everyone has a seat at the table.");
+
 //        Ask user for number of guests attending wedding
         System.out.println("How many guests will be attending the wedding?");
 
@@ -19,16 +24,16 @@ public class WeddingTables {
 //        Calculate how many tables will be filled by the input guests and output table number
 
         int tablesFilled = guests / guestsPerTable;
-        System.out.println("Tables filled: " + tablesFilled);
+        System.out.println("Your guests will fill " + tablesFilled  + " tables.");
 
 //        Determine if there are any extra guests that are not seated at a table
         int extraGuests = guests % guestsPerTable;
-        System.out.println("Guests that need a seat at a table: " + extraGuests);
+        System.out.println(extraGuests + " remaining guests will also need a place to sit.");
 
 //        Determine how many additional tables are needed to seat the extra guests
-        if(extraGuests <= 10) {
+        if(extraGuests > 0 && extraGuests <= 10) {
             int tablesNeeded = tablesFilled + 1;
-            System.out.println("Tables needed: " +  tablesNeeded);
+            System.out.println("So the total tables needed are " +  tablesNeeded + ".");
         }
 
     }
